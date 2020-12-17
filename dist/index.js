462a63f8f3b1cf8058a54db5381475e0f2694bdc
Aleksander Kulinski <Alkowskey@gmail.com>
2020-12-17 15:30:18 +0100

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,22 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const app = express_1.default();
+const port = 8080; // default port to listen
+// Configure views
+app.set("views", path_1.default.join(__dirname, "views"));
+app.set("view engine", "ejs");
+// define a route handler for the default home page
+app.get("/", (req, res) => {
+    res.render("index");
+});
+// start the Express server
+app.listen(port, () => {
+    // tslint:disable-next-line:no-console
+    console.log(`server started at http://localhost:${port}`);
+});
+//# sourceMappingURL=index.js.map
